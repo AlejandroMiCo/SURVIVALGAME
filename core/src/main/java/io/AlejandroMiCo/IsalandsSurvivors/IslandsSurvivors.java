ded9f9894964c5f51a71b8e0dcd66b4379d85d28
Alex <alejandromirandaco1801@gmail.com>
2025-01-24 18:34:37 +0100

    Creacion del primer mundo y sus colisiones

--- core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/IslandsSurvivors.java
+++ core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/IslandsSurvivors.java
@@ -10,8 +10,8 @@ import io.AlejandroMiCo.IsalandsSurvivors.Screens.PlayScreen;
  * platforms.
  */
 public class IslandsSurvivors extends Game {
-    public static final int V_WIDTH = 400;
-    public static final int V_HEIGHT = 208;
+    public static final int V_WIDTH = 800;
+    public static final int V_HEIGHT = 480;
 
     public SpriteBatch batch;
 
--- core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/Screens/PlayScreen.java
+++ core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/Screens/PlayScreen.java
@@ -4,6 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -16,11 +30,45 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
     public PlayScreen(IslandsSurvivors game) {
         this.game = game;
         gameCamera = new OrthographicCamera();
         gamePort = new FitViewport(IslandsSurvivors.V_WIDTH, IslandsSurvivors.V_HEIGHT, gameCamera);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("maps/ProvisionalMap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        for (MapObject object : map.getLayers().get("CollisionLayer").getObjects()
+                .getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
     @Override
@@ -28,18 +76,32 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gameCamera.position.x += 100 * dt;
+        }
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        gameCamera.update();
+        renderer.setView(gameCamera);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
+
+        renderer.render();
+
+        b2dr.render(world, gameCamera.combined);
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
-
-        // game.batch.begin();
-
-        // game.batch.end();
     }
 
     @Override
