6ffdb29bdb84c372982475b9f72447256cbc84c7
Alex <alejandromirandaco1801@gmail.com>
2025-01-24 17:04:03 +0100

    Se me quedo del commit anterior

--- core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/IslandsSurvivors.java
+++ core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/IslandsSurvivors.java
@@ -1,29 +1,30 @@
-package io.AlejandroMiCo.SurvivalGame;
+package io.AlejandroMiCo.IsalandsSurvivors;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import io.AlejandroMiCo.IsalandsSurvivors.Screens.PlayScreen;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
  * platforms.
  */
-public class Main extends Game {
+public class IslandsSurvivors extends Game {
     public SpriteBatch batch;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        setScreen(new GameScreen(this));
+        setScreen(new PlayScreen(this));
     }
 
     @Override
     public void render() {
-        super.render(); // Llama al método render() de la pantalla actual
+        super.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        getScreen().dispose();
     }
 }
