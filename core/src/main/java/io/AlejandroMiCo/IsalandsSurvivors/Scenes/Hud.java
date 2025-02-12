6517c03952c2d6892cefbd9b690d839b90dd8b90
Alejandro <alejandromirandaco1801@gmail.com>
2025-02-12 16:17:58 +0100

    Remove commented-out label code from Hud class

--- core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/Scenes/Hud.java
+++ core/src/main/java/io/AlejandroMiCo/IsalandsSurvivors/Scenes/Hud.java
@@ -41,24 +41,10 @@ public class Hud implements Disposable {
 
         countLabel = new Label(String.format("%02d:%02d", worldTimer / 60, worldTimer % 60),
                 new Label.LabelStyle(new BitmapFont(), com.badlogic.gdx.graphics.Color.ROYAL));
-        // scoreLabel = new Label(String.format("%06d", score),
-        // new Label.LabelStyle(new BitmapFont(),
-        // com.badlogic.gdx.graphics.Color.WHITE));
         timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), com.badlogic.gdx.graphics.Color.WHITE));
-        // levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(),
-        // com.badlogic.gdx.graphics.Color.WHITE));
-        // worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(),
-        // com.badlogic.gdx.graphics.Color.WHITE));
-        // islandsSurvivorsLabel = new Label("KNIGHT",
-        // new Label.LabelStyle(new BitmapFont(),
-        // com.badlogic.gdx.graphics.Color.WHITE));
 
-        // table.add(islandsSurvivorsLabel).expandX().padTop(10);
-        // table.add(worldLabel).expandX().padTop(10);
         table.add(timeLabel).expandX().padTop(10);
         table.row();
-        // table.add(scoreLabel).expandX();
-        // table.add(levelLabel).expandX();
         table.add(countLabel).expandX();
 
         stage.addActor(table);
